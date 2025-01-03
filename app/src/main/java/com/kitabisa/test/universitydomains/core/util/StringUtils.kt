c82f1e0836b06d6d0f54abcea40622b7ef7a7a12
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-03 10:29:30 +0700

    feat(utils): add Constants, IntentUtils, and countryCodeToEmojiFlag
    - Introduce Constants.kt with Urls object for GitHub link
    - Add IntentUtils for opening URLs using CustomTabsIntent
    - Provide countryCodeToEmojiFlag in StringUtils.kt to convert country codes into flag emojis

---
+++ app/src/main/java/com/kitabisa/test/universitydomains/core/util/StringUtils.kt
@@ -0,0 +1,17 @@
+package com.kitabisa.test.universitydomains.core.util
+
+import java.util.Locale
+
+fun countryCodeToEmojiFlag(countryCode: String): String {
+    return countryCode
+        .uppercase(Locale.US)
+        .map { char ->
+            Character.codePointAt("$char", 0) - 0x41 + 0x1F1E6
+        }
+        .map { codePoint ->
+            Character.toChars(codePoint)
+        }
+        .joinToString(separator = "") { charArray ->
+            String(charArray)
+        }
+}
