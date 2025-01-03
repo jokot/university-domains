7be51366b1ac21b97864f7c2ed9b92f05cef3ae1
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-03 10:23:25 +0700

    feat(datastore): implement getFavorites and toggleFavorite in LocalDataSourceImpl
    - Add logic to retrieve current favorites from DataStore
    - Implement toggleFavorite for adding or removing IDs from favorites

--- app/src/main/java/com/kitabisa/test/universitydomains/core/datastore/LocalDataSource.kt
+++ app/src/main/java/com/kitabisa/test/universitydomains/core/datastore/LocalDataSource.kt
@@ -2,11 +2,13 @@ package com.kitabisa.test.universitydomains.core.datastore
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 interface LocalDataSource {
-    fun getFavorites(): Flow<Set<String>>
+    fun getFavorites(): Flow<Set<Int>>
     suspend fun toggleFavorite(id: Int, isFavorite: Boolean)
     fun getIsFetch(): Flow<Boolean>
     suspend fun setDataIsFetch()
@@ -14,13 +16,27 @@ interface LocalDataSource {
 
 class LocalDataSourceImpl @Inject constructor(
     private val dataStore: DataStore<Preferences>
-): LocalDataSource {
-    override fun getFavorites(): Flow<Set<String>> {
-        TODO("Not yet implemented")
-    }
+) : LocalDataSource {
+    override fun getFavorites(): Flow<Set<Int>> = dataStore.data
+        .map { preferences ->
+            preferences[DatastoreKey.FAVORITES]
+                .orEmpty()
+                .mapNotNull { it.toIntOrNull() }
+                .toSet()
+        }
 
     override suspend fun toggleFavorite(id: Int, isFavorite: Boolean) {
-        TODO("Not yet implemented")
+        dataStore.edit { preferences ->
+            val currentFavorites = preferences[DatastoreKey.FAVORITES].orEmpty().toMutableSet()
+
+            if (isFavorite) {
+                currentFavorites.add(id.toString())
+            } else {
+                currentFavorites.remove(id.toString())
+            }
+
+            preferences[DatastoreKey.FAVORITES] = currentFavorites
+        }
     }
 
     override fun getIsFetch(): Flow<Boolean> {
