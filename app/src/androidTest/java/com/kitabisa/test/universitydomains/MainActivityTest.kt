17722f75ebb4891414dc9c7a227ed90fff5d15a8
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-03 19:52:03 +0700

    test(ui): add MainActivityTest, introduce testTag, and relocate TestRunner
    - Add MainActivityTest to verify that the UniversityApp in main activity is displayed correctly
    - Introduce testTag in UniversityApp for consistent UI component identification in tests
    - Move TestRunner from core.testing to core.testing.util for better organization and utility management

---
+++ app/src/androidTest/java/com/kitabisa/test/universitydomains/MainActivityTest.kt
@@ -0,0 +1,33 @@
+package com.kitabisa.test.universitydomains
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import com.kitabisa.test.universitydomains.core.testing.constant.TestTag
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class MainActivityTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun appAreDisplayed() {
+        composeTestRule
+            .onNodeWithTag(TestTag.UNIVERSITY_APP)
+            .assertIsDisplayed()
+    }
+}
--- app/src/main/java/com/kitabisa/test/universitydomains/ui/UniversityApp.kt
+++ app/src/main/java/com/kitabisa/test/universitydomains/ui/UniversityApp.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import com.kitabisa.test.universitydomains.core.testing.constant.TestTag
 import com.kitabisa.test.universitydomains.core.ui.component.Appbar
 import com.kitabisa.test.universitydomains.navigation.UniversityNavHost
 
@@ -21,6 +23,7 @@ fun UniversityApp(
     val destination = appState.currentTopLevelDestination
 
     Scaffold(
+        modifier = Modifier.testTag(TestTag.UNIVERSITY_APP),
         topBar = {
             Appbar(title = stringResource(destination.titleId))
         },
