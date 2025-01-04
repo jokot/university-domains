d32a17cbd1a2c874e00426dd4b429db220581ff9
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-04 08:10:45 +0700

    feat(ui): add AsyncImage to AppBar with ICON_URL
    - Introduce `ICON_URL` in `Constants.Urls` for the app icon URL.
    - Add Coil's `AsyncImage` to `AppBar`, displaying the icon before the title text.
    - Adjust layout and spacing in `AppBar` for a seamless integration of the icon and title.

--- app/src/main/java/com/kitabisa/test/universitydomains/core/ui/component/Appbar.kt
+++ app/src/main/java/com/kitabisa/test/universitydomains/core/ui/component/Appbar.kt
@@ -1,6 +1,10 @@
 package com.kitabisa.test.universitydomains.core.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -8,10 +12,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.kitabisa.test.universitydomains.R
 import com.kitabisa.test.universitydomains.core.ui.icon.UniversityIcons
 import com.kitabisa.test.universitydomains.core.ui.theme.UniversityDomainsTheme
@@ -29,12 +37,26 @@ fun Appbar(
         modifier = modifier,
         windowInsets = WindowInsets(0, 0, 0, 0),
         title = {
-            Text(title, style = MaterialTheme.typography.titleLarge)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                AsyncImage(
+                    model = Urls.ICON_URL,
+                    contentDescription = "App Icon",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clip(CircleShape)
+                )
+                Text(title, style = MaterialTheme.typography.titleLarge)
+            }
         },
         actions = {
-            IconButton(onClick = {
-                IntentUtils.openUrl(context, Urls.GITHUB_URL)
-            }) {
+            IconButton(
+                onClick = {
+                    IntentUtils.openUrl(context, Urls.GITHUB_URL)
+                }
+            ) {
                 Icon(
                     imageVector = UniversityIcons.Info,
                     contentDescription = "info"
--- app/src/main/java/com/kitabisa/test/universitydomains/core/util/Constants.kt
+++ app/src/main/java/com/kitabisa/test/universitydomains/core/util/Constants.kt
@@ -2,4 +2,5 @@ package com.kitabisa.test.universitydomains.core.util
 
 object Urls {
     const val GITHUB_URL = "https://github.com/jokot/university-domains"
+    const val ICON_URL = "https://i.ibb.co.com/s2ZzkKd/distance-education.webp"
 }
